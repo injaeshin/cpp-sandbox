6dbd054d662dc90ac8cc1a55ff869fba1571b04d
InJae Shin <iwishonmystar@gmail.com>
2021-05-24 23:12:30 +0900

    Hello##1

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,16 +1,18 @@
 #include "Windows.h"
+
+#include <iostream>
+
 #include "MiniDump.h"
 
 int main()
 {
     MiniDump::Init();
 
+
     int z = 0;
     int a = 10;
-    int b = a / 1;
 
-
-    int c = b / z;
+    std::cout << a / z;
 
     return 0;
 }
